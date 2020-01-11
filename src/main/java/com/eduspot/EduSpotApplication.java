33fdb554255b4fa1172115595c4a9919921dfd93
Venomicon <jangajda9@gmail.com>
2020-01-11 15:08:16 +0100

    Initial commit

---
+++ src/main/java/com/eduspot/EduSpotApplication.java
@@ -0,0 +1,13 @@
+package com.eduspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduSpotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduSpotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eduspot/EduSpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduSpotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
